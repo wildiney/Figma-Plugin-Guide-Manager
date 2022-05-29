ec33c243d93fa51eee64936a66610fbe5526b0ca
Wildiney Di Masi <wildiney@gmail.com>
2022-05-29 20:05:20 -0300

    Initial Commit

---
+++ code.ts
@@ -0,0 +1,69 @@
+/* eslint-disable no-undef */
+
+const windowWidth = 320
+const windowHeight = 300
+
+figma.showUI(__html__, { themeColors: true, width: windowWidth, height: windowHeight })
+
+function addGuides (sel: any, guide: any) {
+  const selection = sel
+  selection.guides = selection.guides.concat(guide)
+}
+
+figma.ui.onmessage = msg => {
+  switch (msg.type) {
+    case 'add-guides':
+      figma.currentPage.selection.forEach((sel) => {
+        console.log(sel)
+        const selection = sel as FrameNode
+        const guideData = msg.data
+        console.log(msg.data)
+
+        if (selection.type === 'FRAME') {
+          addGuides(selection, guideData)
+        } else {
+          figma.notify('Please select a Frame')
+        }
+      })
+      break
+    case 'add-margins':
+      figma.currentPage.selection.forEach((sel) => {
+        const selection = sel as FrameNode
+        const marginSize = parseInt(msg.data.marginSize)
+        const height = selection.height
+        const width = selection.width
+        const margins = [{
+          top: marginSize,
+          bottom: height - marginSize,
+          left: marginSize,
+          right: width - marginSize
+        }]
+        console.log(margins)
+        if (selection.type === 'FRAME') {
+          for (const margin of margins) {
+            addGuides(selection, { axis: 'Y', offset: margin.top })
+            addGuides(selection, { axis: 'Y', offset: margin.bottom })
+            addGuides(selection, { axis: 'X', offset: margin.left })
+            addGuides(selection, { axis: 'X', offset: margin.right })
+          }
+        } else {
+          figma.notify('Please select a Frame')
+        }
+      })
+      break
+    case 'clearAllGuides':
+      console.log('clear all guides')
+      if (figma.currentPage.selection.length > 0) {
+        figma.currentPage.selection.forEach((sel) => {
+          const selection = sel as FrameNode
+          selection.guides = []
+        })
+      } else {
+        figma.notify('Please select a Frame')
+      }
+      break
+    case 'close':
+      figma.closePlugin()
+      break
+  }
+}
