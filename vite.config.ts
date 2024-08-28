0cd3dac2d7d12fa7472c75446b9709bd275e87d0
Wildiney Di Masi <wildiney@gmail.com>
2024-08-28 15:03:52 -0300

    remove tests

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 import { viteSingleFile } from 'vite-plugin-singlefile'
-import { defineConfig, coverageConfigDefaults } from 'vitest/config'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   root: './ui-src',
-  plugins: [reactRefresh(), viteSingleFile()],
+  plugins: [react(), viteSingleFile()],
   build: {
     target: 'esnext',
     assetsInlineLimit: 100000000,
@@ -18,18 +19,4 @@ export default defineConfig({
       },
     },
   },
-  test: {
-    environment: 'jsdom',
-    globals: true,
-    setupFiles: './ui-src/setupTests.ts', // Caminho para o arquivo setup
-    include: ['**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'], // include your test files
-    coverage: {
-      exclude: [
-        '**/assets/**',
-        '**/main.tsx',
-        '**/App.tsx',
-        ...coverageConfigDefaults.exclude,
-      ],
-    }
-  }
 })
