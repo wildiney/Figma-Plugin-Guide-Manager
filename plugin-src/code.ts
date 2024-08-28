a9fc08684853dce5dad2bf5403896f4ea13c90e1
Wildiney Di Masi <wildiney@gmail.com>
2024-08-28 14:26:53 -0300

    refactor file to ensure better practices and less repeated code

--- plugin-src/code.ts
+++ plugin-src/code.ts
@@ -1,107 +1,94 @@
-const windowWidth = 340
+const windowWidth = 360
 const windowHeight = 642
 
-figma.showUI(__html__, { themeColors: true, width: windowWidth, height: windowHeight })
+figma.showUI(__html__, {
+  themeColors: true,
+  width: windowWidth,
+  height: windowHeight,
+})
 
-function addGuides (sel: any, guide: any) {
-  const selection = sel
+export function addGuides (selection: FrameNode, guide: any) {
   selection.guides = selection.guides.concat(guide)
 }
 
-figma.on('selectionchange', () => {
+function handleSelectionChange () {
   const selected = figma.currentPage.selection
 
-  if (selected != undefined && selected.length > 0) {
+  if (selected && selected.length > 0) {
     figma.ui.postMessage({ type: 'selectionchange', status: 'selected' })
-    console.log(selected)
   } else {
     figma.ui.postMessage({ type: 'selectionchange', status: 'none' })
   }
-})
+}
 
-figma.ui.onmessage = msg => {
-  let value = 0
+function handleAddGuide (selection: FrameNode, guideData: any) {
+  if (selection.type === 'FRAME') {
+    addGuides(selection, guideData)
+  } else {
+    figma.notify('Please, select a frame')
+  }
+}
+
+function handleAddMargins (selection: FrameNode, marginSize: number) {
+  const { height, width } = selection
+  const margins = [
+    { axis: 'Y', offset: marginSize },
+    { axis: 'Y', offset: height - marginSize },
+    { axis: 'X', offset: marginSize },
+    { axis: 'X', offset: width - marginSize },
+  ]
+  margins.forEach((margin) => addGuides(selection, margin))
+}
+
+function handleClearAllGuides (selection: FrameNode) {
+  selection.guides = []
+}
+function handleFrameSize (selection: FrameNode, dimension: 'width' | 'height') {
+  return selection[dimension]
+}
+
+figma.on('selectionchange', handleSelectionChange)
+
+figma.ui.onmessage = (msg) => {
+  const selectedFrames = figma.currentPage.selection.filter(
+    (sel) => sel.type === 'FRAME'
+  ) as FrameNode[]
+
+  if (selectedFrames.length <= 0) {
+    figma.notify('Please, select a frame')
+  }
 
   switch (msg.type) {
     case 'add-guide':
-      figma.currentPage.selection.forEach((sel) => {
-        console.log(sel)
-        const selection = sel as FrameNode
-        const guideData = msg.data
-        console.log(msg.data)
-
-        if (selection.type === 'FRAME') {
-          addGuides(selection, guideData)
-        } else {
-          figma.notify('Please select a Frame')
-        }
-      })
+      selectedFrames.forEach((selection) => handleAddGuide(selection, msg.data))
       break
     case 'add-margins':
-      if (figma.currentPage.selection.length == 0) {
-        figma.notify("Please select a frame")
+      const marginSize = parseInt(msg.data.marginSize, 10)
+      if (isNaN(marginSize)) {
+        figma.notify('Invalid margin size')
+        return
       }
-      figma.currentPage.selection.forEach((sel) => {
-        const selection = sel
-        const marginSize = parseInt(msg.data.marginSize)
-        const height = selection.height
-        const width = selection.width
-        const margins = [{
-          top: marginSize,
-          bottom: height - marginSize,
-          left: marginSize,
-          right: width - marginSize
-        }]
-        console.log(margins)
-        if (selection.type === 'FRAME') {
-          for (const margin of margins) {
-            addGuides(selection, { axis: 'Y', offset: margin.top })
-            addGuides(selection, { axis: 'Y', offset: margin.bottom })
-            addGuides(selection, { axis: 'X', offset: margin.left })
-            addGuides(selection, { axis: 'X', offset: margin.right })
-          }
-        } else {
-          figma.notify('Please select a Frame')
-        }
-      })
+      selectedFrames.forEach((selection) =>
+        handleAddMargins(selection, marginSize)
+      )
       break
     case 'clearAllGuides':
-      console.log('clear all guides')
-      if (figma.currentPage.selection.length > 0) {
-        figma.currentPage.selection.forEach((sel) => {
-          const selection = sel as FrameNode
-          selection.guides = []
-        })
-      } else {
-        figma.notify('Please select a Frame')
-      }
+      selectedFrames.forEach(handleClearAllGuides)
       break
     case 'frameWidth':
-      console.log('server frame width')
-      if (figma.currentPage.selection.length > 0) {
-        figma.currentPage.selection.forEach((sel) => {
-          const selection = sel as FrameNode
-          value = selection.width
-        })
-      } else {
-        figma.notify('Please select a Frame')
-      }
-      figma.ui.postMessage({ query: 'inputValue', width: value })
-      break
     case 'frameHeight':
-      console.log('server frame width')
-      if (figma.currentPage.selection.length > 0) {
-        figma.currentPage.selection.forEach((sel) => {
-          const selection = sel as FrameNode
-          value = selection.height
-        })
-      } else {
-        figma.notify('Please select a Frame')
-      }
-      figma.ui.postMessage({ query: 'inputValue', height: value })
+      console.log(msg.type)
+      const dimension = msg.type === 'frameWidth' ? 'width' : 'height'
+      const value =
+        selectedFrames.length > 0
+          ? handleFrameSize(selectedFrames[0], dimension)
+          : 0
+      figma.ui.postMessage({ query: 'inputValue', [dimension]: value })
       break
     case 'close':
       figma.closePlugin()
       break
+    default:
+      figma.notify('Please, select a frame')
   }
 }
